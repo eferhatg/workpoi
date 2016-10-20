ac3b66c42be4f89082d293867534f571bec0e088
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2016-10-20 22:52:53 +0300

    template filename changed

--- handlers/home.go
+++ handlers/home.go
@@ -12,6 +12,6 @@ func GetHome(c *gin.Context) {
 	db:=c.MustGet("db").(*sqlx.DB)
 	v:=models.NewVenue(db)
 	vr,_:=v.GetOne()
-	c.HTML(http.StatusOK, "home.html.tmpl", vr)
+	c.HTML(http.StatusOK, "home.tmpl", vr)
 	
 }
